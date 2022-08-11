ae369a77cb4bcbe4707d486194416d5d435ea102
Jesús Álvarez <khisus19@gmail.com>
2022-08-11 14:42:32 -0500

    Navbar ready

---
+++ my-travel-journal-solo-project/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import "../styles/Navbar.css"
+
+export default function Navbar() {
+  return( 
+    <nav className="nav-container">
+      <img src="../../src/assets/earth-icon.svg" alt="Earth icon" />
+      <p>my travel journal.</p>
+    </nav>
+  )
+}
