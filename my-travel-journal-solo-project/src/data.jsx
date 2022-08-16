2cba683f315152565e0b6fce391ac798b39796d2
Jesús Álvarez <khisus19@gmail.com>
2022-08-16 15:55:19 -0500

    Replacing data with stadiums and styling it properly

--- my-travel-journal-solo-project/src/data.jsx
+++ my-travel-journal-solo-project/src/data.jsx
@@ -1,42 +1,52 @@
 export default [
   {
     id: 1,
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
-    description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    title: "Santiago Bernabeu",
+    location: "Spain",
+    googleMapsUrl: "https://g.page/EstadioRealMadrid?share",
+    startDate: "12 Nov, 2021",
+    endDate: "18 Nov, 2021",
+    description: "With a current seating capacity of 81,044, it has been the home stadium of Real Madrid since its completion in 1947. It is the second-largest stadium in Spain and third-largest home to a top-flight European club after Camp Nou and Westfalenstadion. Named after footballer and legendary Real Madrid president Santiago Bernabéu (1895-1978), the stadium is one of the world's most famous football venues.",
+    imageUrl: "https://source.unsplash.com/LDuFjsin71k"
   },
   {
     id: 2,
-    title: "Sydney Opera House",
-    location: "Australia",
-    googleMapsUrl: "https://goo.gl/maps/wLpGDp8marjiPYYV7",
-    startDate: "27 May, 2021",
-    endDate: "8 Jun, 2021",
-    description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-    imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+    title: "Allianz Arena",
+    location: "Germany",
+    googleMapsUrl: "https://g.page/FCBAllianzArena?share",
+    startDate: "02 Oct, 2023",
+    endDate: "07 Oct, 2023",
+    description: "Allianz Arena (known as Fußball Arena München for UEFA competitions) is a football stadium in Munich, Bavaria, Germany with a 75,000 seating capacity for domestic matches. Widely known for its exterior of inflated ETFE plastic panels, it is the first stadium in the world with a full colour changing exterior.",
+    imageUrl: "https://source.unsplash.com/ifSQkr5NHeU"
   },
   {
     id: 3,
-    title: "Geirangerfjord",
-    location: "Norway",
-    googleMapsUrl: "https://goo.gl/maps/yM4Q8EYyomPjrcMZ6",
-    startDate: "01 Oct, 2021",
+    title: "Spotify Camp Nou",
+    location: "Spain",
+    googleMapsUrl: "https://goo.gl/maps/QoGg1L1is84GrBmHA",
+    startDate: "18 Nov, 2021",
     endDate: "18 Nov, 2021",
-    description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    description: "Camp Nou (catalan meaning new field), officially branded as Spotify Camp Nou for sponsorship reasons, is a football stadium in Barcelona, Spain. It has been the home stadium of FC Barcelona since its completion in 1957. With a current seating capacity of 99,354, it is the largest stadium in Spain and Europe.",
+    imageUrl: "https://source.unsplash.com/gT5Aa9PBAtk"
   },
   {
     id: 4,
-    title: "Teotihuacan",
-    location: "Mexico",
-    googleMapsUrl: "https://goo.gl/maps/XTfgsuxjD1Bi6UA77",
-    startDate: "02 Oct, 2023",
-    endDate: "07 Oct, 2023",
-    description: "The holy city of Teotihuacan ('the place where the gods were created') was built between the 1st and 7th centuries A.D., it is characterized by the vast size of its monuments, in particular, the Temple of Quetzalcoatl and the Pyramids of the Sun and the Moon, laid out on geometric and symbolic principles.",
-    imageUrl: "https://source.unsplash.com/OKlgGGanlwY"
-  }
+    title: "Old Trafford",
+    location: "England",
+    googleMapsUrl: "https://goo.gl/maps/KTbkBYAh3FXdwjSU6",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description: "Old Trafford is a football stadium in Old Trafford, Greater Manchester, England, and the home of Manchester United. With a capacity of 74,310 it is the largest club football stadium (and second-largest football stadium overall after Wembley Stadium) in the United Kingdom, and the eleventh-largest in Europe.",
+    imageUrl: "https://source.unsplash.com/_DYnUjTEUnM"
+  },
+  {
+    id: 5,
+    title: "Wembley",
+    location: "England",
+    googleMapsUrl: "https://goo.gl/maps/opwkhQCawRS9HK9h7",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
+    description: "It opened in 2007 on the site of the original Wembley Stadium, which was demolished from 2002 to 2003. The stadium hosts major football matches including home matches of the England national football team, and the FA Cup Final.",
+    imageUrl: "https://source.unsplash.com/g9SNY0aLMF0"
+  },
 ]
