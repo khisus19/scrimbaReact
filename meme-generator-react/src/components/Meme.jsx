0be0fd895df7fb68bd022b2ec6d0a70807a3beec
Jesús Álvarez <khisus19@gmail.com>
2022-08-18 22:05:00 -0500

    Meme component styled and functional

--- meme-generator-react/src/components/Meme.jsx
+++ meme-generator-react/src/components/Meme.jsx
@@ -2,38 +2,61 @@ import React from "react"
 import "../styles/Meme.css"
 
 export default function Meme() {
-  const [meme, SetMeme] = React.useState({
+  const [meme, setMeme] = React.useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg"
   })
-  const [allMemes, setAllMemes] = React.useState({})
+  const [allMemes, setAllMemes] = React.useState([])
 
-  /* function fetchMemes() {
+  React.useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
-      .then(res => res.json())
-      .then(data => )
-  } */
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+  }, [])
+
+  function getNewMeme() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length)
+      const url = allMemes[randomNumber].url
+      setMeme(prevMeme => ({
+          ...prevMeme,
+          randomImage: url
+      }))
+  }
+
+  function handleChange(event) {
+    const {name, value} = event.target
+    setMeme(prevMeme => ({
+        ...prevMeme,
+        [name]: value
+    }))
+}
 
   return(
     <main>
-      <form>
-        <button>Get a new image</button>
+      <section className="form">
+        <button
+          onClick={getNewMeme}
+          >Get a new image</button>
         <input
           type="text"
           placeholder="Top text"
           name="topText"
+          value={meme.topText}
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="Bottom text"
           name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
         />
-      </form>
+      </section>
       <div className="meme__container">
         <img src={meme.randomImage} />
-        <h2 className="meme__top-text">One does not simply</h2>
-        <h2 className="meme__bottom-text">Walks into mordor</h2>
+        <h2 className="meme__top-text">{meme.topText}</h2>
+        <h2 className="meme__bottom-text">{meme.bottomText}</h2>
       </div>
     </main>
   )
